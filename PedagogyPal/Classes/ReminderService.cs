6c5e6e8e6473fd9a39a40b7c615ecca17c5152a4
NaeemBrown <102594804+NaeemBrown@users.noreply.github.com>
2025-07-25 13:22:23 +0200

    Update ReminderService.cs

--- PedagogyPal/Classes/ReminderService.cs
+++ PedagogyPal/Classes/ReminderService.cs
@@ -94,13 +94,6 @@ namespace PedagogyPal.Services
             }
         }
 
-        // Override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources.
-        // ~ReminderService()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
         public void Dispose()
         {
             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
